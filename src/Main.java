9b81a86a4ab8ccae987bfefcb0f499bf970f49b6
Alex Kleschenko <alexkv2694@gmail.com>
2021-08-06 14:33:29 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int account = 2000;
+        int topUp = 1300;
+        if (topUp > 1000) {
+            int bonus = (topUp) / 100;
+            System.out.println(account +topUp + bonus);
+            System.out.println(bonus);
+        } else {
+            System.out.println(account);
+        }
+
+    }
+}
