17a5f8c1693077f6b576a74f6cbe2fb1fd06a06b
Alex Kleschenko <alexkv2694@gmail.com>
2021-08-11 14:08:34 +0300

    update code

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         int account = 2000;
-        int topUp = 1300;
-        if (topUp > 1000) {
+        int topUp = 100;
+         {
             int bonus = (topUp) / 100;
             System.out.println(account + topUp + bonus);
             System.out.println(bonus);
