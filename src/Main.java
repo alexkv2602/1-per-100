c083fa8e62981e89314a1385b9fb610f873061ea
alexkv2602 <84174553+alexkv2602@users.noreply.github.com>
2021-08-11 14:05:09 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int Account = 2000;
-        int TopUp = 1300;
-        if (TopUp > 1000) {
-            int Bonus = (TopUp) / 100;
-            System.out.println(Account + TopUp + Bonus);
-            System.out.println(Bonus);
+        int account = 2000;
+        int topUp = 1300;
+        if (topUp > 1000) {
+            int bonus = (topUp) / 100;
+            System.out.println(account + topUp + bonus);
+            System.out.println(bonus);
         }
     }
 
