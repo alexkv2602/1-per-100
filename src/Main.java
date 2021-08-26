7a1dae571844894fdbca116542f37e396572a5a9
Alex Kleschenko <alexkv2694@gmail.com>
2021-08-26 14:46:35 +0300

    update code

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 1000;
-        int topUpBalance = 2000;
+        int topUpBalance = 700;
         int bonus = topUpBalance / 100;
         int resultBalance;
         if (topUpBalance > 1000) {
@@ -10,6 +10,12 @@ public class Main {
             resultBalance = balance + topUpBalance;
         }
         System.out.println("Остаток на счете:" + resultBalance);
-        System.out.println("Бонус:" + bonus);
+        int resultBonus;
+        if (topUpBalance > 1000) {
+            resultBonus = topUpBalance / 100;
+        } else {
+        resultBonus = 0;
+        }
+        System.out.println("Бонус:" + resultBonus);
     }
 }
